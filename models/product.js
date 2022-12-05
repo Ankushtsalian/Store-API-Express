0ef5cadae0ad4799a605e2e44141da59ed6448a0
Ankush <ankushtsalian@gmail.com>
2022-12-05 19:31:12 +0530

    created schema

---
+++ models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide Product name"],
+  },
+  price: {
+    type: String,
+    required: [true, "Please provide Product Price"],
+  },
+  featurede: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} IS NOT SUPPORTED",
+    },
+    // enum: ["ikea", "liddy", "caressa", "marcos"],
+  },
+});
+
+module.exports = mongoose.model("ProDUCT", productSchema);
