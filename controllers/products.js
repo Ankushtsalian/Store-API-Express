f23356eb082acf45823e5400946daba4baa133e0
Ankush <ankushtsalian@gmail.com>
2022-12-05 20:00:23 +0530

    created find method to gettt all products in db

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,8 @@
+const productSchema = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Product testing route" });
+  const products = await productSchema.find();
+  res.status(200).json({ msg: products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
   res.status(200).json({ msg: "Products route" });
