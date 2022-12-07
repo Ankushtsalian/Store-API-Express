67c1e1f9bcc110fe1fb7a9226d6fe68cc1d3688e
Ankush <ankushtsalian@gmail.com>
2022-12-07 12:24:20 +0530

    created query sting test in db

--- controllers/products.js
+++ controllers/products.js
@@ -10,7 +10,7 @@ const getAllProducts = async (req, res) => {
   //if we have wrong query which isn't in db
   const queryObject = {};
   //just create obj contains valid query
-  if (featured) queryObject.featured = featured ? true : false;
+  if (featured) queryObject.featured = featured === "true" ? true : false;
   //if queryObj is empty get all product
   const products = await productSchema.find(queryObject);
   res.status(200).json({ msg: products, nbHits: products.length });
