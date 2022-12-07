5dc13f2bb4ee292c65a27c29438c5307066001d5
Ankush <ankushtsalian@gmail.com>
2022-12-07 11:50:48 +0530

    created schema and dynamic add prod to db

--- controllers/products.js
+++ controllers/products.js
@@ -2,6 +2,7 @@ const productSchema = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const products = await productSchema.find();
+
   res.status(200).json({ msg: products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
